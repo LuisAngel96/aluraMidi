602b24139da3fc243e292ac503ee91a2edf34677
LuisAngel96 <lavalle1996@gmail.com>
2024-05-03 22:52:06 -0500

    Update js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+const listaDeTeclas =  document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = playPom;
+
 function playPom () {
     document.querySelector("#sonido_tecla_pom").play();
 
 }
 
-document.querySelector('.tecla_pom').onclick = playPom;
